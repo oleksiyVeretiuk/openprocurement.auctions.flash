856fb18ad806036f98b9d55d884a93d197fef67e
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-04 14:40:24 +0300

    Fixes according new cornice version

--- openprocurement/auctions/flash/tests/tender.py
+++ openprocurement/auctions/flash/tests/tender.py
@@ -115,7 +115,7 @@ class AuctionResourceTest(BaseWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Offset expired/invalid', u'location': u'params', u'name': u'offset'}
+            {u'description': u'Offset expired/invalid', u'location': u'querystring', u'name': u'offset'}
         ])
 
         response = self.app.get('/auctions?feed=changes&descending=1&limit=10')
